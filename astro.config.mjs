bd33d3a5d36e94b89f91c82d9ab261a8e06d903c
Ehsan Pourhadi <ehsan.pourhadi@telavox.com>
2024-11-24 14:32:50 +0100

    deploy setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,21 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
 
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    react(),
-    mdx(),
-  ],
+  site: "https://portfolio-astro.github.io",
+  base: "portfolio-astro",
+  integrations: [tailwind(), react(), mdx()],
   markdown: {
     shikiConfig: {
-      theme: 'dracula',
-      wrap: true
-    }
+      theme: "dracula",
+      wrap: true,
+    },
   },
   vite: {
     ssr: {
-      noExternal: ['react-chartjs-2']
-    }
-  }
+      noExternal: ["react-chartjs-2"],
+    },
+  },
 });
