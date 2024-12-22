21400eb222435a451b50f464032e1db995ad5761
Ehsan Pourhadi <ehsan.po@gmail.com>
2024-12-22 04:40:50 +0100

    try fixing build

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,26 +2,27 @@ import { defineCollection, z } from "astro:content";
 
 const portfolio = defineCollection({
   type: "content",
-  schema: z.object({
-    title: z.string(),
-    date: z.string(),
-    status: z.string(),
-    permalink: z.string(),
-    author: z.string(),
-    type: z.string(),
-    id: z.number(),
-    agancy: z.string(),
-    category: z.array(z.string()),
-    tag: z.array(z.string()),
-    case_link_url: z.array(z.string()),
-    client: z.array(z.string()),
-    tagline: z.array(z.string()),
-    background_image: z.array(z.string()),
-    logo: z.array(z.string()),
-    bilder: z.array(z.string()).optional(),
-    port_date: z.array(z.string()),
-    onHome: z.boolean().optional(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      date: z.string(),
+      status: z.string(),
+      permalink: z.string(),
+      author: z.string(),
+      type: z.string(),
+      id: z.number(),
+      agancy: z.string(),
+      category: z.array(z.string()),
+      tag: z.array(z.string()),
+      case_link_url: z.array(z.string()),
+      client: z.array(z.string()),
+      tagline: z.array(z.string()),
+      background_image: z.array(z.string()),
+      logo: z.array(z.string()),
+      bilder: z.array(image()).optional(),
+      port_date: z.array(z.string()),
+      onHome: z.boolean().optional(),
+    }),
 });
 
 export const collections = {
